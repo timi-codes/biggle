d21f995170ed878f06e860997a7d2e216f325b51
--replace <tejumoladavid@gmail.com>
2024-05-03 20:01:58 -0400

    setup monorepo

---
+++ apps/web/src/app/celeb/[id]/page.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+
+const CelebrityPage = () => {
+    return (
+        <div>
+            Tasha
+        </div>
+    )
+}
+
+export default CelebrityPage;
---
+++ apps/web/src/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "ui";
+// import { useHelloQuery } from "../store/services/api";
+
+export default function Web() {
+//   const { data } = useHelloQuery();
+
+  return (
+    <div>
+      <h1>hOME</h1>
+      <Button />
+    </div>
+  );
+}
